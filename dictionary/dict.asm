c82a40af7dcd26e5633be77ca03e5398e8013a75
slayer404 <doomer888@protonmail.com>
2021-11-26 13:57:07 +0300

    function for working with list

--- dictionary/dict.asm
+++ dictionary/dict.asm
@@ -2,19 +2,32 @@ extern string_equals
 
 global find_word
 
+%define offset 8
+
 section .text
 
 find_word:
+
 .loop:
+    push rdi
     push rsi 
+    add rsi, offset  
     call string_equals
-    test rax, rax
-    jz .loop
-.word_found:
     pop rsi
+    pop rdi
+    test rax, rax
+    jnz .word_found
+    mov rsi, [rsi] 
+    test rsi, rsi
+    jz .word_not_found
+    jmp .loop
+
+.word_found:
     mov rax, rsi
     jmp .end 
+
 .word_not_found: 
-    
+    mov rax, 0
+
 .end: 
     ret  
